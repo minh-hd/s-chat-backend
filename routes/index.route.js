2b722962b0a5a84b03e2caf0df182764a2882e6d
Hoang Duy Minh <hoangduy.minh18@gmail.com>
2023-03-05 18:28:27 +0700

    feat: add hello world

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,6 +3,10 @@ import { encode } from '../middlewares/jwt.js';
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  return res.send('Hello world');
+});
+
 router.post('/login', encode, (req, res, next) => {
   return res.status(200).json({
     success: true,
